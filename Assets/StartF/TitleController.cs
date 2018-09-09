4915ed63ab078f4a9c73602b0d3a169e42d0fc88
menntai <menntai0923@icloud.com>
2018-09-09 10:41:55 +0900

    画面遷移

---
+++ Assets/StartF/TitleController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TitleController : MonoBehaviour {
+
+    //効果音
+    private AudioSource audioSource;        // AudioSorceを格納する変数の宣言.
+    public List<AudioClip> audioClip = new List<AudioClip>();
+    
+
+    void Start()
+    {
+        //audioSource = gameObject.AddComponent<AudioSource>();
+
+    }
+
+    public void OnStartButtonClicked()
+    {
+        Debug.Log("ccc");
+        //ボタンを押したら音を鳴らして下の関数へ
+        //audioSource.PlayOneShot(audioClip[0]);
+        //Application.LoadLevel("select");
+        //StartCoroutine("GoToSelect");
+		Application.LoadLevel("Contact");
+    }
+
+}
